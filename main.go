042622a85686e477a081a2decd5e60c16d9cd2af
AxelPilop <axelaxel741@gmail.com>
2025-07-13 14:33:02 -0400

    feat: add streaming recommendations endpoint with basic API validation

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ const logo = `
 func main() {
 	app := &cli.App{
 		Name:    "chaturbate-dvr",
-		Version: "2025.28.2",
+		Version: "2025.28.5",
 		Usage:   "Record your favorite Chaturbate streams automatically. 😎🫵",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
